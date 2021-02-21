0be94982dc9f2adb0db0a8cd033c3de5508ef91d
Diorgenes Luan de Menezes Backes <diorgenes.backes@agibank.com.br>
2021-02-21 04:00:57 -0300

    adicionando health check, liveness e readiness

--- server.go
+++ server.go
@@ -4,23 +4,42 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
+	http.HandleFunc("/healthz", Healthz)
 	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	age := os.Getenv("AGE")
+var startedAt = time.Now()
 
-	fmt.Fprintf(w, "Hello Success! Name: %s, Age: %s.", name, age)
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Success!")
 }
 
 func Secret(w http.ResponseWriter, r *http.Request) {
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
 	fmt.Fprintf(w, "Hello Success! User: %s, Password: %s.", user, password)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 20 {
+		w.WriteHeader(500)
+		w.Write([]byte(fmt.Sprintf("Ainda não ta pronto: %v", duration.Seconds())))
+	} else {
+		if duration.Seconds() > 40 {
+			w.WriteHeader(500)
+			w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+		} else {
+			w.WriteHeader(200)
+			w.Write([]byte("Tudo belezinha por aqui"))
+		}
+	}
+
 }
