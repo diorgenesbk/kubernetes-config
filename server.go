0cac79f13ba24a4e3668c2e14dfb2c59add30df2
Diorgenes Luan de Menezes Backes <diorgenes.backes@agibank.com.br>
2021-02-21 02:20:41 -0300

    integrando com secret

--- server.go
+++ server.go
@@ -1,18 +1,26 @@
 package main
+
 import (
-	"fmt"	
+	"fmt"
 	"net/http"
 	"os"
 )
 
-func main(){
+func main() {
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
 
-func Hello(w http.ResponseWriter, r *http.Request){
+func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
-	
+
 	fmt.Fprintf(w, "Hello Success! Name: %s, Age: %s.", name, age)
+}
+
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "Hello Success! User: %s, Password: %s.", user, password)
 }
